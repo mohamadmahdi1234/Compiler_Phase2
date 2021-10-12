6fb479619ad251d1d78a37070f3646aeb811a819
NeginPardaz <NeginPardaz@example.com>
2021-10-12 22:39:40 +0330

    constructor added to pre_processor\

--- src/Pre_Processor.java
+++ src/Pre_Processor.java
@@ -1,2 +1,12 @@
+import java.util.regex.Pattern;
+
 public class Pre_Processor {
+    private Pattern all_define_expression;
+    private Pattern first_two_par_of_define;
+    String input;
+
+    public Pre_Processor(String input) {
+        this.input = input;
+    }
+
 }
