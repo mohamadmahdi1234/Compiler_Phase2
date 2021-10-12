047f955fcd564e84d79e285a6110d8f08b214a18
NeginPardaz <NeginPardaz@example.com>
2021-10-13 01:02:22 +0330

    change_defined_value method comppleted

--- src/Pre_Processor.java
+++ src/Pre_Processor.java
@@ -8,12 +8,16 @@ public class Pre_Processor {
     private Pattern first_two_par_of_define;
     private Map<String,String> define_value;
     String input;
+    String holder;
+    String holder1;
 
     public Pre_Processor(String input) {
         this.input = input;
         first_two_par_of_define=Pattern.compile("(?<= |^)define\\s+[A-Za-z]+[a-zA-Z0-9]*");
         all_define_expression=Pattern.compile("(?<= |^)define\\s+[A-Za-z]+[a-zA-Z0-9]*.*");
-       define_value=new HashMap<>();
+        holder="(?<= |^|\\[|\\]|\\(|\\)|[+ * \\- / % < <= > >= = += \\-= *= /= == != \\&& \\|| ! ; , \\.])";
+        holder1="(?= |^|\\[|\\]|\\(|\\)|[+ * \\- / % < <= > >= = += \\-= *= /= == != \\&& \\|| ! ; , \\.])";
+        define_value=new HashMap<>();
     }
     public String handle_define(){
         find_defines();
@@ -34,7 +38,26 @@ public class Pre_Processor {
         input=input.trim();
     }
     public void change_defined_values(){
-
+        for (String key:define_value.keySet()) {
+            Pattern defined_in_string=Pattern.compile("\"[^\"]*"+holder+key+holder1+"[^\"]*\"");
+            Pattern defined_variable=Pattern.compile(holder+key+holder1);
+            Matcher first=defined_in_string.matcher(input);
+            Matcher second=defined_variable.matcher(input);
+            while (second.find()){
+                if(first.find()){
+                    continue;
+                }
+                if(second.group().length()!=key.length()){
+                String[]s=second.group().split(key);
+                input=input.substring(0,second.start())+s[0]+define_value.get(key)+s[1]+input.substring(second.end());
+                }else{
+                    input=input.substring(0,second.start())+define_value.get(key)+input.substring(second.end());
+                }
+                first=defined_in_string.matcher(input);
+                second=defined_variable.matcher(input);
+            }
+        }
+        input.trim();
     }
 
     public static void main(String[] args) {
