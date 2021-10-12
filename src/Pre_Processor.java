4eb1de88fd4e62b23eb604a91fc44f55c18a3f64
NeginPardaz <NeginPardaz@example.com>
2021-10-12 22:46:22 +0330

    add defination of two attribute(patterns) to pre_processor class

--- src/Pre_Processor.java
+++ src/Pre_Processor.java
@@ -7,6 +7,8 @@ public class Pre_Processor {
 
     public Pre_Processor(String input) {
         this.input = input;
+        first_two_par_of_define=Pattern.compile("(?<= |^)define\\s+[A-Za-z]+[a-zA-Z0-9]*");
+        all_define_expression=Pattern.compile("(?<= |^)define\\s+[A-Za-z]+[a-zA-Z0-9]*.*");
     }
     public String handle_define(){
         find_defines();
