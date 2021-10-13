dab7133308bc150807bc9098af102c6906551208
NeginPardaz <NeginPardaz@example.com>
2021-10-13 09:33:31 +0330

    put Main class of script in own class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            System.out.println("Usage: java Main -i <input> -o <output>");
+            return;
+        }
+        String inputFileName = null;
+        String outputFileName = null;
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].equals("-i")) {
+                    inputFileName = args[i + 1];
+                }
+                if (args[i].equals("-o")) {
+                    outputFileName = args[i + 1];
+                }
+            }
+        }
+        String outputPath = "out/" + outputFileName;
+        createFile(outputPath);
+
+        // Read the input file and write to the output file.
+
+        String[] lines = {
+                "class",
+                "T_ID Program",
+                "{",
+                "void",
+                "T_ID main",
+                "(",
+                ")",
+                "{",
+                "}",
+                "}"
+        };
+
+        writeContentToFile(outputPath, lines);
+    }
+
+    private static boolean createFile(String path) {
+        File file = new File(path);
+        try {
+            return file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void writeContentToFile(String path, String[] lines) {
+        try (FileWriter writer = new FileWriter(new File(path))) {
+            String content = String.join("\n", lines);
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
